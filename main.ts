4c59de051620da6205dffd4199adb718d6320559
Jaguar151611 <122543765+Jaguar151611@users.noreply.github.com>
2023-01-26 09:38:56 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    variable += 1
+    basic.showNumber(variable,45)
+})
+input.onButtonPressed(Button.AB, function () {
+    variable = 0
+    basic.showNumber(variable)
+})
+input.onButtonPressed(Button.B, function () {
+    variable += -1
+    basic.showNumber(variable)
+})
+let variable = 0
+variable = 0
+basic.showNumber(variable)
